058490c46f53eeaf52639022275a5161ab5863b5
Amirul Aidil <90623526+Mirulaidl@users.noreply.github.com>
2024-11-12 13:19:34 +0800

    SettingUpPages

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h2>About Me</h2>
+        <p>I am a student with skills in web development, interested in creating dynamic and responsive websites.</p>
+        <ul>
+            <li>Skill 1: HTML/CSS</li>
+            <li>Skill 2: JavaScript</li>
+            <li>Skill 3: Laravel</li>
+        </ul>
+    </div>
+</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+    <h1>Welcome to My Profile</h1>
+    <p>This is a brief introduction about me.</p>
+</div>
+@endsection
