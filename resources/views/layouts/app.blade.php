e52d9fb0de73f41c25ee83add9ba3fdddcca69ff
hamzah <hamzahismid3@gmail.com>
2024-11-13 18:53:40 +0800

    home and app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,10 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Profile</a>
+        <!-- <a class="navbar-brand" href="/">Profile</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
-        </button>
+        </button> -->
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
